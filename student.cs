6cd2a6bbb84ad404f360d2d0f9e0246b82341103
Nancy <nmccoy176@gmail.com>
2016-08-17 16:45:30 -0400

    Create student.cs

---
+++ student.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+	
+namespace Computer_Science_Student
+{
+   abstract class Student
+    {
+       // Fields
+       private string _name;
+       private string _id;
+
+       // Constructor
+       public Student(string name, string id)
+       {
+           _name = name;
+           _id = id;
+       }
+
+       // Name property
+       public string name
+       {
+           get { return _name; }
+           set { _name = value; }
+       }
+
+       // ID property
+       public string ID
+       {
+           get { return _id; }
+           set { _id = value; }
+       }
+
+       // RequiredHours property (abstract)
+       public abstract double RequiredHours
+       {
+           get;
+       }
+   }
+
+
+    }
+
